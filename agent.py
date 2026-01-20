830af7cbda4e656bdc5e40eb7e77c9a5d821e509
Pramod Vemulapalli <v.pramodkrishna@gmail.com>
2026-01-19 22:35:38 -0600

    Connect to MCP Server and create LangChain agent

--- agent.py
+++ agent.py
@@ -3,6 +3,8 @@ from langchain_mcp_adapters.client import MultiServerMCPClient
 from langchain.agents import create_agent
 
 async def main():
+
+    // connect to an MCP Server
     client = MultiServerMCPClient(
         {
             "my_mcp_server": {
@@ -11,10 +13,13 @@ async def main():
             }
         }
     )
+    
     # Load/convert MCP tools -> LangChain tools
     tools = await client.get_tools()
+    
     # Create a LangChain agent that can call those tools
     agent = create_agent("openai:gpt-4.1", tools)
+    
     # Run it
     result = await agent.ainvoke(
         {"messages": "Use the MCP tools to help me to generate a sales research report"}
